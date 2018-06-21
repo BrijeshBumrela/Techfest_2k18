528f0c1d7a67d1754caad49ab165dc486c0f214e
Adwait Thattey <coderdude1999@gmail.com>
2018-06-21 11:49:39 +0530

    Updated urls.py
    Modified urls.py to serve media files

--- Techfest_2k18/urls.py
+++ Techfest_2k18/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import main_page.views
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index', main_page.views.index),
     path('', main_page.views.redirect_to_index),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
