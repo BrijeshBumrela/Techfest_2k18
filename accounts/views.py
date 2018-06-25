1584db5a9f0f3d21780bc3071e34012fb18a1a01
coder-dude <coderdude1999@gmail.com>
2018-06-25 10:42:45 +0530

    Partially created register_for_event view

--- accounts/views.py
+++ accounts/views.py
@@ -65,3 +65,8 @@ def edit_additional_info(request):
             new_form = MoreUserDataForm()
 
         return render(request, "accounts/edit_additional_info.html", {"data_form": new_form})
+
+
+@login_required
+def register_user_for_event(request) :
+    pass
