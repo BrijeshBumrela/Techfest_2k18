f87932064335c418a3cd16768c430c117dedac8c
coder-dude <coderdude1999@gmail.com>
2018-06-25 20:59:40 +0530

    Partially completed T inheritance

--- Techfest_2k18/urls.py
+++ Techfest_2k18/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 import main_page.views
 from . import settings
 from django.conf.urls.static import static
@@ -22,7 +22,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index', main_page.views.index),
-    path('', main_page.views.redirect_to_index),
+    path('', main_page.views.redirect_to_index , name="home"),
+    path('skeleton', main_page.views.skeleton),
+    path('index2',main_page.views.index2)
 ]
 
 if settings.DEBUG:
