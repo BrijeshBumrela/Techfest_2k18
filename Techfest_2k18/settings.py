5e64070a81c211f94f93fd50cf476ae027eebdee
coder-dude <coderdude1999@gmail.com>
2018-06-28 14:19:22 +0530

    Setup app widget-tweaks

--- Techfest_2k18/settings.py
+++ Techfest_2k18/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
+    'widget_tweaks',
     'data',
     'main_page',
     'registration',
