ca76cdfb061dd28cc75e59a02e8c0e0aa83b3a6a
coder-dude <coderdude1999@gmail.com>
2018-06-28 13:19:12 +0530

    Creating register fr event view I

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,10 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
-    path('profile', views.redirect_to_profile_home,),
+    path('profile', views.redirect_to_profile_home, ),
     path('profile/', views.redirect_to_profile_home),
     path('profile/edit-additional-info', views.edit_additional_info, name="edit_additional_info"),
     path('profile/home', views.profile_home, name="profile_home"),
-    path('login/', auth_views.login, name="login")
+    path('login/', auth_views.login, name="login"),
+    path('event/<slug:event_name>/register', views.register_user_for_event, name="register_for_event"),
 ]
