57b0a405342c12d5c2b0ec974052af154d8172e3
Vineet-Sharma29 <35156299+Vineet-Sharma29@users.noreply.github.com>
2018-07-01 19:25:15 +0530

    Made changes to make slider cover page width

--- main_page/static/main_page/js/jR3Djsfile.js
+++ main_page/static/main_page/js/jR3Djsfile.js
@@ -7,21 +7,21 @@ $(document).ready(function(){
 	//var jR3DCarousel;
 	
 	jR3DCarousel = $('.jR3DCarouselGallery').jR3DCarousel({
-		width: 470, 		/* largest allowed width */
-		height: 272, 		/* largest allowed height */
+		width: 770, 		/* largest allowed width */
+		height: 400, 		/* largest allowed height */
 		slides: slideImages /* array of images source */
 	});
 	
 	var carouselCustomeTemplateProps =  {
-	 		  width: 470, 				/* largest allowed width */
-			  height: 272, 				/* largest allowed height */
+	 		  width: 770, 				/* largest allowed width */
+			  height: 400, 				/* largest allowed height */
 			  slideLayout : 'fill',     /* "contain" (fit according to aspect ratio), "fill" (stretches object to fill) and "cover" (overflows box but maintains ratio) */
 			  animation: 'slide3D', 	/* slide | scroll | fade | zoomInSlide | zoomInScroll */
 			  animationCurve: 'ease',
 			  animationDuration: 1900,
 			  animationInterval: 2000,
 			  slideClass: 'jR3DCarouselCustomSlide',
-			  autoplay: false,
+			  autoplay: true,
 			  controls: true,			/* control buttons */
 			  navigation: ''			/* circles | squares | '' */,
 			  perspective: 2200,
