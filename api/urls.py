ac7c2138995f750cd22fb6252859dc868f642e96
Anubhav <anubhavujjawal@gmail.com>
2018-07-20 23:49:14 +0530

    added registration for events

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
-from .views import EventsList
+from .views import EventsList, MoreUserDataView, RegisteredOrNotView
 
 app_name = "api"
 
 urlpatterns = [
     path('api-token-auth', obtain_jwt_token),
     path('EventsList', EventsList.as_view(), name="EventsList"),
+    path('MoreUserDataView', MoreUserDataView.as_view(), name="MoreUserDataView"),
+    path('RegisteredOrNotView', RegisteredOrNotView.as_view(), name="RegisteredOrNotView"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from api.serializers import EventSerializer
-from data.models import Event
+from api.serializers import EventSerializer, MoreUserDataSerializer
+from data.models import Event, MoreUserData
 
 class EventsList(ListAPIView):
     queryset = Event.objects.all().order_by('end_date_time', 'start_date_time')
     serializer_class = EventSerializer
     permission_classes = (AllowAny,)
 
+
+class MoreUserDataView(APIView):
+    
+    def get(self, request, format=None):
+        user_data = request.user.moreuserdata
+        serializer = MoreUserDataSerializer(user_data)
+        return Response(serializer.data)
+ 
+
+class RegisteredOrNotView(APIView):
+    def get(self, request, format=None):
+        obj = get_object_or_404(Event, id=request.GET.get('id'))
+        user_data = request.user.moreuserdata
+        if user_data in obj.participants.all():
+            return Response({
+                "participating": 1
+            })
+        else:
+            return Response({
+                "participating": 0
+            })
+
+    def post(self, request, format=None):
+        obj = get_object_or_404(Event, id=request.data.get('id'))
+        user_data = request.user.moreuserdata
+        if user_data not in obj.participants.all():
+            obj.participants.add(user_data)
+        return Response(status=200)
+
+    
